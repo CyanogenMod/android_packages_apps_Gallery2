67bd49ad9f817484fdba0686e11cc1ed489ccf91
Bryan Gantt <dragonzkiller@gmail.com>
2012-10-16 09:42:04 -0300

    Added system to build LegacyCamera if specified
    Must be built with the LegacyCamera commit that moves PanoramaActivity
    to the root directory.
    
    Change-Id: Id59e72ada6893c6f6de2b2f486a637509a50d71d

--- Android.mk
+++ Android.mk
@@ -9,9 +9,18 @@ LOCAL_STATIC_JAVA_LIBRARIES += com.android.gallery3d.common2
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
-LOCAL_SRC_FILES += $(call all-java-files-under, ../Camera/src)
+ifeq (,$(findstring LegacyCamera,$(PRODUCT_PACKAGES)))
+LOCAL_SRC_FILES += $(call all-java-files-under, packages/apps/LegacyCamera/src)
+else
+LOCAL_SRC_FILES += $(call all-java-files-under, packages/apps/Camera/src)
+endif
 
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res packages/apps/Camera/res
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+ifeq (,$(findstring LegacyCamera,$(PRODUCT_PACKAGES)))
+LOCAL_RESOURCE_DIR += packages/apps/LegacyCamera/res
+else
+LOCAL_RESOURCE_DIR += packages/apps/Camera/res
+endif
 LOCAL_AAPT_FLAGS := --auto-add-overlay --extra-packages com.android.camera
 
 LOCAL_PACKAGE_NAME := Gallery2
@@ -20,21 +29,39 @@ LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 
 #LOCAL_SDK_VERSION := current
 
-LOCAL_JNI_SHARED_LIBRARIES := libjni_mosaic libjni_eglfence
+ifeq (,$(findstring LegacyCamera,$(PRODUCT_PACKAGES)))
+LOCAL_JNI_SHARED_LIBRARIES := libjni_legacymosaic
+else
+LOCAL_JNI_SHARED_LIBRARIES := libjni_mosaic
+endif
+LOCAL_JNI_SHARED_LIBRARIES := libjni_eglfence
 
-LOCAL_REQUIRED_MODULES := libjni_mosaic libjni_eglfence
+ifeq (,$(findstring LegacyCamera,$(PRODUCT_PACKAGES)))
+LOCAL_REQUIRED_MODULES := libjni_legacymosaic
+else
+LOCAL_REQUIRED_MODULES := libjni_mosaic
+endif
+LOCAL_REQUIRED_MODULES := libjni_eglfence
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
+ifeq (,$(findstring LegacyCamera,$(PRODUCT_PACKAGES)))
+include $(call all-makefiles-under, packages/apps/LegacyCamera/jni)
+else
 include $(call all-makefiles-under, jni)
+endif
 
 ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
 # Use the following include to make gallery test apk.
 include $(call all-makefiles-under, $(LOCAL_PATH))
 
 # Use the following include to make camera test apk.
-include $(call all-makefiles-under, ../Camera)
+ifeq (,$(findstring LegacyCamera,$(PRODUCT_PACKAGES)))
+include $(call all-makefiles-under, packages/apps/LegacyCamera)
+else
+include $(call all-makefiles-under, packages/apps/Camera)
+endif
 
 endif
