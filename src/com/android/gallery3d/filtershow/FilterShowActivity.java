b363a613f98e97441215a6dd01c9ed8b6f28abf0
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-01-30 11:58:20 -0500

    Gallery2: Localize strings
    Undo and redo were hardcoded in the photo editor. This allows their
    localization.
    
    Change-Id: I63df977696b74128ca5e6fc45037b6b43d43a465

--- src/com/android/gallery3d/filtershow/FilterShowActivity.java
+++ src/com/android/gallery3d/filtershow/FilterShowActivity.java
@@ -601,7 +601,7 @@ public class FilterShowActivity extends Activity implements OnItemClickListener,
                 HistoryAdapter adapter = mImageShow.getHistory();
                 int position = adapter.undo();
                 mImageShow.onItemClick(position);
-                mImageShow.showToast("Undo");
+                mImageShow.showToast(getString(R.string.filtershow_undo));
                 invalidateViews();
                 return true;
             }
@@ -609,7 +609,7 @@ public class FilterShowActivity extends Activity implements OnItemClickListener,
                 HistoryAdapter adapter = mImageShow.getHistory();
                 int position = adapter.redo();
                 mImageShow.onItemClick(position);
-                mImageShow.showToast("Redo");
+                mImageShow.showToast(getString(R.string.filtershow_redo));
                 invalidateViews();
                 return true;
             }
