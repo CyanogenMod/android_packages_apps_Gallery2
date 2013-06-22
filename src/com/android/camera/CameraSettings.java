6945dd2a0db1809c0cf05bfbfbb83815ceec89b5
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-22 21:37:20 +0530

    Add a setting for Shutter Speed
    Squashed commit of the following commits from cm-10.1 camera:
    d51009325d70b87e9061097bff3def6a76d7bbae Add a setting for Shutter Speed
    67f48445fd895baff250c65ab77226be402ade1b Util: remove redundant check for ShutterSpeed
    69a931c45f364d53fcd1605dc1153ecb4e6242f2 show the actual shutter speed
    
    * Removed config, automatically enable settings when parameter is present
    * Moved to enhance submenu
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ieebe50a4be04e7fea9301da63c008e36f4ec0dbd

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -82,6 +82,7 @@ public class CameraSettings {
     public static final String KEY_BURST_MODE = "pref_camera_burst_key";
     public static final String KEY_BEAUTY_MODE = "pref_camera_beauty_mode";
     public static final String KEY_SLOW_SHUTTER = "pref_camera_slow_shutter";
+    public static final String KEY_SHUTTER_SPEED = "pref_camera_shutter_speed_key";
 
     public static final String EXPOSURE_DEFAULT_VALUE = "0";
     public static final String VALUE_ON = "on";
@@ -200,6 +201,7 @@ public class CameraSettings {
         ListPreference videoColorEffect = group.findPreference(KEY_VIDEOCAMERA_COLOR_EFFECT);
         ListPreference beautyMode = group.findPreference(KEY_BEAUTY_MODE);
         ListPreference slowShutter = group.findPreference(KEY_SLOW_SHUTTER);
+        ListPreference shutterSpeed = group.findPreference(KEY_SHUTTER_SPEED);
 
         // Since the screen could be loaded from different resources, we need
         // to check if the preference is available here
@@ -286,6 +288,9 @@ public class CameraSettings {
         }
         if (storage != null) {
             buildStorage(group, storage);
+        }
+        if (shutterSpeed != null && !Util.isShutterSpeedSupported(mParameters)) {
+            removePreference(group, shutterSpeed.getKey());
         }
     }
 
