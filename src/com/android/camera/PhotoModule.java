bbedf36c22db3c1766857c192be873305cf4ebf5
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-07-18 14:40:48 -0700

    camera: fix image capture review mode when zsl is enabled
    Choosing "Take Photo" to change a contact image doesn't show
    the captured imaged after taking a snap if ZSL is enabled.
    Also observed on apps that use the native camera to capture
    an image like Whatsapp.
    
    In ZSL mode, the preview is not stopped, due to which the
    review mode (ImageCapture) doesn't show the captured frame.
    Hence stop the preview if ZSL mode is active so that the
    preview can be restarted using the onReviewRetakeClicked().
    
    Also, reset the autofocus and set the camera to idle only
    when the ZSL mode is active. This fixes issues with non ZSL
    cameras where autofocus works even during the review mode
    after taking the snap since the camera state is set to idle.
    
    Change-Id: I2aa3f4146575329c6fefb259b09937108084fdab

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -861,6 +861,12 @@ public class PhotoModule
                     // time before starting the preview.
                     mHandler.sendEmptyMessageDelayed(SETUP_PREVIEW, 300);
                 }
+            } else if (mIsImageCaptureIntent && Util.isZSLEnabled()) {
+                // In ZSL mode, the preview is not stopped, due to which the
+                // review mode (ImageCapture) doesn't show the captured frame.
+                // Hence stop the preview if ZSL mode is active so that the
+                // preview can be restarted using the onReviewRetakeClicked().
+                stopPreview();
             } else {
                 mFocusManager.resetTouchFocus();
                 setCameraState(IDLE);
