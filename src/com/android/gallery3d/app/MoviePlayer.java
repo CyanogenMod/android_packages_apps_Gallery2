5167fdf5b42e471a51a3bc44ca1a5adb69e676e2
Likai Ding <likaid@codeaurora.org>
2013-07-22 10:36:14 +0800

    Gallery2: support audio effects during video play.
    Porting from kk with fixes
    
    Conflicts:
    	res/values/strings.xml
    
    Change-Id: I267c3c7e402fd5cf2bb223547003af491dd8289e
    Signed-off-by: Xiaojing Zhang <zhangx@codeaurora.org>

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -476,6 +476,14 @@ public class MoviePlayer implements
             if (mVideoView.isPlaying()) pauseVideo();
         }
     }
+
+    public int getAudioSessionId() {
+        return mVideoView.getAudioSessionId();
+    }
+
+    public void setOnPreparedListener(MediaPlayer.OnPreparedListener listener) {
+        mVideoView.setOnPreparedListener(listener);
+    }
 }
 
 class Bookmarker {
