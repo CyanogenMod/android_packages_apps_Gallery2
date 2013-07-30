b67255618b634a370fa4fdfb95e53cd030a8b50b
kaiyiz <kaiyiz@codeaurora.org>
2013-07-30 16:29:59 +0800

    Gallery2: if phone is locked, pause the video, if not, start it.
    If there was a video playing before locking the phone,
    After pressing the emergency call option in lock screen,
    and then presses back key, the MovieActivity will resume,
    so the video will play background.
    
    Set a listener, if Keyguard is locked,pause the video,
    if not, start it.
    
    CRs-Fixed: 427283
    
    Change-Id: Ia825afee691ee53f53eebf1f96efe09ef88ca743

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -18,6 +18,7 @@ package com.android.gallery3d.app;
 
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -88,6 +89,9 @@ public class MoviePlayer implements
     private long mResumeableTime = Long.MAX_VALUE;
     private int mVideoPosition = 0;
     private boolean mHasPaused = false;
+    private boolean mVideoHasPaused = false;
+    private boolean mCanResumed = false;
+    private boolean mKeyguardLocked = false;
     private int mLastSystemUiVis = 0;
 
     // If the time bar is being dragged.
@@ -114,6 +118,21 @@ public class MoviePlayer implements
         public void run() {
             int pos = setProgress();
             mHandler.postDelayed(mProgressChecker, 1000 - (pos % 1000));
+        }
+    };
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                mKeyguardLocked = true;
+            } else if (Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
+                if ((mCanResumed) && (!mVideoHasPaused)) {
+                    playVideo();
+                }
+                mKeyguardLocked = false;
+                mCanResumed = false;
+            }
         }
     };
 
@@ -184,6 +203,12 @@ public class MoviePlayer implements
 
         mAudioBecomingNoisyReceiver = new AudioBecomingNoisyReceiver();
         mAudioBecomingNoisyReceiver.register();
+
+        // Listen for broadcasts related to user-presence
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_USER_PRESENT);
+        mContext.registerReceiver(mReceiver, filter);
 
         Intent i = new Intent(SERVICECMD);
         i.putExtra(CMDNAME, CMDPAUSE);
@@ -294,6 +319,10 @@ public class MoviePlayer implements
             // If we have slept for too long, pause the play
             if (System.currentTimeMillis() > mResumeableTime) {
                 pauseVideo();
+            } else if (mKeyguardLocked){
+                // If Keyguard Locked , pause the play
+                mCanResumed = true;
+                mVideoView.pause();
             }
         }
         mHandler.post(mProgressChecker);
@@ -306,6 +335,7 @@ public class MoviePlayer implements
         }
         mVideoView.stopPlayback();
         mAudioBecomingNoisyReceiver.unregister();
+        mContext.unregisterReceiver(mReceiver);
     }
 
     // This updates the time bar display (if necessary). It is called every
@@ -342,12 +372,14 @@ public class MoviePlayer implements
         mVideoView.start();
         mController.showPlaying();
         setProgress();
+        mVideoHasPaused = false;
     }
 
     private void pauseVideo() {
         mVideoView.pause();
         setProgress();
         mController.showPaused();
+        mVideoHasPaused = true;
     }
 
     // Below are notifications from VideoView
