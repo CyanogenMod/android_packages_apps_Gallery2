8c7a515ed64fa4fee3d867f28cc391f7f0f548f9
lbdroid <aserbinski@gmail.com>
2013-08-07 13:29:52 -0400

    Always use recording dimensions for preview
    Some devices, like apexq, require the preview dimensions to be the same as the recording dimensions.
    
    PS2: that was dumb.
    PS2: rebase
    
    Change-Id: I307fb4c0d4df37abe0a045a69693f480022fc597

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -711,7 +711,8 @@ public class VideoModule implements CameraModule,
     private void getDesiredPreviewSize() {
         mParameters = mActivity.mCameraDevice.getParameters();
         if (ApiHelper.HAS_GET_SUPPORTED_VIDEO_SIZE) {
-            if (mParameters.getSupportedVideoSizes() == null || cantUsePreviewSizeDueToEffects()) {
+            if (mParameters.getSupportedVideoSizes() == null || cantUsePreviewSizeDueToEffects() ||
+                    mActivity.getResources().getBoolean(R.bool.useVideoSizeForPreview)) {
                 mDesiredPreviewWidth = mProfile.videoFrameWidth;
                 mDesiredPreviewHeight = mProfile.videoFrameHeight;
             } else {  // Driver supports separates outputs for preview and video.
