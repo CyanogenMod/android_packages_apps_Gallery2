f86795803bd163fd488fdcf886d1200bf2ef4666
wenbinw <wenbinw@codeaurora.org>
2013-08-08 10:34:12 +0800

    Camera: Fix swiping disabled after unsupported video prompt
    Before recording starts, some parameters will be checked.
    If failed, the recording will not run. So the swiping disable
    should be done after this check.
    
    Change-Id: I3e6b0bf1f5d5eb8eef333dac79e56e01bd51b43d
    CRs-Fixed: 523885

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1662,8 +1662,6 @@ public class VideoModule implements CameraModule,
 
     private void startVideoRecording() {
         Log.v(TAG, "startVideoRecording");
-        mUI.enablePreviewThumb(false);
-        mActivity.setSwipingEnabled(false);
 
         mActivity.updateStorageSpaceAndHint();
         if (mActivity.getStorageSpace() <= Storage.LOW_STORAGE_THRESHOLD) {
@@ -1708,6 +1706,9 @@ public class VideoModule implements CameraModule,
                 return;
             }
         }
+
+        mUI.enablePreviewThumb(false);
+        mActivity.setSwipingEnabled(false);
 
         // Make sure the video recording has started before announcing
         // this in accessibility.
