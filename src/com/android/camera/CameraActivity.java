148d371e3fff32318cdff34803b992c5fe82c469
chiz <chiz@codeaurora.org>
2013-09-17 09:10:08 +0800

    Camera: Fix crash during open and close camera
    When we open camera for the second time after opening camera,
    switching to camcorder and exit camcorder rapidly, crash will
    be observed. It's due to the assertion of double open.
    When app receives pause event, it should cancel the switch
    animation, as we as it's listener.
    
    Change-Id: Id351d3e5db63c54646d592b728a4624e3d6e1211

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -323,6 +323,9 @@ public class CameraActivity extends ActivityBase
 
     @Override
     public void onPause() {
+        if (mCameraSwitchAnimator != null && mCameraSwitchAnimator.isRunning()) {
+            mCameraSwitchAnimator.cancel();
+        }
         mPaused = true;
         mOrientationListener.disable();
         mCurrentModule.onPauseBeforeSuper();
