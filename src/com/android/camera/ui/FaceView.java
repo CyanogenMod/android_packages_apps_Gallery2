61d30cb4286ec7192107017f66eea8038e472219
Steve Kondik <shade@chemlab.org>
2013-11-13 01:25:55 -0800

    camera: Handle alternate face detection score
     * Some cameras use the score field as an error code. Add a config
       to handle this properly.
    
    Change-Id: Iad938e5c31c8a34fc90b1da4b096ce027294e89e

--- src/com/android/camera/ui/FaceView.java
+++ src/com/android/camera/ui/FaceView.java
@@ -66,6 +66,8 @@ public class FaceView extends View
     private Paint mPaint;
     private volatile boolean mBlocked;
 
+    private final boolean mFaceScoreIsError;
+
     private int mUncroppedWidth;
     private int mUncroppedHeight;
 
@@ -103,6 +105,8 @@ public class FaceView extends View
         mPaint.setDither(true);
         mPaint.setColor(Color.WHITE);//setColor(0xFFFFFF00);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        mFaceScoreIsError = context.getResources().getBoolean(R.bool.faceScoreIsError);
     }
 
     public void setFaces(Face[] faces) {
@@ -220,7 +224,10 @@ public class FaceView extends View
             canvas.rotate(-mOrientation); // rotate is counter-clockwise (for canvas)
             for (int i = 0; i < mFaces.length; i++) {
               // Filter out false positives.
-              if (mFaces[i].score < 50) continue;
+              if (!mFaceScoreIsError && mFaces[i].score < 50)
+                  continue;
+              if (mFaceScoreIsError && mFaces[i].score > 0)
+                  continue;
 
               // Transform the coordinates.
               mRect.set(mFaces[i].rect);
