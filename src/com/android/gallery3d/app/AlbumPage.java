c0ca5c121ebc584c1d4227d74b2b6c08e4de6fa3
yanglv <yanglv@codeaurora.org>
2013-11-14 17:13:08 +0800

    Gallery2: Remove slideshow option if all files of the folder are video
    Enter Gallery and open folder whose files are all video, press
    menu key should no slideshow option.
    
    Add condition to remove slideshow if the folder are all video
    files.
    
    CRs-Fixed: 574984
    
    Change-Id: Ib1603f1609b6490e44039ab64abefb44f14bd477

--- src/com/android/gallery3d/app/AlbumPage.java
+++ src/com/android/gallery3d/app/AlbumPage.java
@@ -59,6 +59,7 @@ import com.android.gallery3d.util.Future;
 import com.android.gallery3d.util.GalleryUtils;
 import com.android.gallery3d.util.MediaSetUtils;
 
+import java.util.ArrayList;
 
 public class AlbumPage extends ActivityState implements GalleryActionBar.ClusterRunner,
         SelectionManager.SelectionListener, MediaSet.SyncListener, GalleryActionBar.OnAlbumModeSelectedListener {
@@ -115,6 +116,8 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
 
     private Handler mHandler;
     private static final int MSG_PICK_PHOTO = 0;
+
+    private Menu mActionMenu;
 
     private PhotoFallbackEffect mResumeEffect;
     private PhotoFallbackEffect.PositionProvider mPositionProvider =
@@ -540,6 +543,7 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
 
     @Override
     protected boolean onCreateActionBar(Menu menu) {
+        mActionMenu = menu;
         GalleryActionBar actionBar = mActivity.getGalleryActionBar();
         MenuInflater inflator = getSupportMenuInflater();
         if (mGetContent) {
@@ -560,6 +564,26 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
 
         }
         actionBar.setSubtitle(null);
+        // remove slideshow if all are videos
+        if (allVideoFiles() && !mGetContent){
+            menu.findItem(R.id.action_slideshow).setVisible(false);
+        }
+        return true;
+    }
+
+    private boolean allVideoFiles() {
+        if (mMediaSet == null)
+            return false;
+        int count = mMediaSet.getMediaItemCount();
+        MediaItem item;
+        for (int i = 0; i < count; i++) {
+            item = mMediaSet.getMediaItem(i, 1).get(0);
+            if (item == null) {
+                continue;
+            }
+            if (item.getMimeType().trim().startsWith("image/"))
+                return false;
+        }
         return true;
     }
 
@@ -652,6 +676,12 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
         }
     }
 
+    private void updateMenuItem() {
+        if (allVideoFiles() && !mGetContent) {
+            mActionMenu.findItem(R.id.action_slideshow).setVisible(false);
+        }
+    }
+
     @Override
     public void onSelectionModeChange(int mode) {
         switch (mode) {
@@ -663,6 +693,7 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
             case SelectionManager.LEAVE_SELECTION_MODE: {
                 mActionModeHandler.finishActionMode();
                 mRootPane.invalidate();
+                updateMenuItem();
                 break;
             }
             case SelectionManager.SELECT_ALL_MODE: {
