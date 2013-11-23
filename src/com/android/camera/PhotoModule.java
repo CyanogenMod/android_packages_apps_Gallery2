c623609d1e91b52d522533217021e15001b72fe1
Abdul Razak <baginda86@gmail.com>
2013-11-23 21:15:15 +0000

    Revert "Revert "camera: improve snapshot KPI""
    This reverts commit 445732cd2f0cd193781cc35ed38e606d6e3c8724.
    
    Change-Id: Ia99ecd2e909e55ebd66789e6ed226c30152b1fa9

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -784,6 +784,14 @@ public class PhotoModule
             mRawPictureCallbackTime = System.currentTimeMillis();
             Log.v(TAG, "mShutterToRawCallbackTime = "
                     + (mRawPictureCallbackTime - mShutterCallbackTime) + "ms");
+            if (ApiHelper.HAS_SURFACE_TEXTURE && !mIsImageCaptureIntent
+                    && mActivity.mShowCameraAppView) {
+                // Finish capture animation
+                mHandler.removeMessages(CAPTURE_ANIMATION_DONE);
+                ((CameraScreenNail) mActivity.mCameraScreenNail).animateSlide();
+                mHandler.sendEmptyMessageDelayed(CAPTURE_ANIMATION_DONE,
+                        CaptureAnimManager.getAnimationDuration());
+            }
         }
     }
 
@@ -826,14 +834,6 @@ public class PhotoModule
             // Only animate when in full screen capture mode
             // i.e. If monkey/a user swipes to the gallery during picture taking,
             // don't show animation
-            if (ApiHelper.HAS_SURFACE_TEXTURE && !mIsImageCaptureIntent
-                    && mActivity.mShowCameraAppView) {
-                // Finish capture animation
-                mHandler.removeMessages(CAPTURE_ANIMATION_DONE);
-                ((CameraScreenNail) mActivity.mCameraScreenNail).animateSlide();
-                mHandler.sendEmptyMessageDelayed(CAPTURE_ANIMATION_DONE,
-                        CaptureAnimManager.getAnimationDuration());
-            }
             mFocusManager.updateFocusUI(); // Ensure focus indicator is hidden.
 
             boolean isSamsungHDR =
