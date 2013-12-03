0b9e20a63f96830080897fef45de3e424e70c2af
kayiz <kaiyiz@codeaurora.org>
2013-12-03 15:04:01 +0800

    Gallery2: fix FC when view the details of some pictures
    Pictures captured by some devices seems didn't comply
    with the standards, it will get null when set details,
    then it will throw out an exception.
    
    If it can not get a detail value, do not display it,
    and print error log.
    
    CRs-Fixed: 583463
    
    Change-Id: I69be48cb25a4413ae7ddbe301891c94e88671c0f

--- src/com/android/gallery3d/ui/DialogDetailsView.java
+++ src/com/android/gallery3d/ui/DialogDetailsView.java
@@ -201,8 +201,9 @@ public class DialogDetailsView implements DetailsViewContainer {
                         Object valueObj = detail.getValue();
                         // This shouldn't happen, log its key to help us diagnose the problem.
                         if (valueObj == null) {
-                            Utils.fail("%s's value is Null",
-                                    DetailsHelper.getDetailsName(context, detail.getKey()));
+                            Log.e(TAG, DetailsHelper.getDetailsName(
+                                    context, detail.getKey()) + "'s value is Null");
+                            continue;
                         }
                         value = valueObj.toString();
                     }
