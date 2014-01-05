eb38c879822254bb1546675e6d8b85f1c117b0ac
Scott Warner <Tortel1210@gmail.com>
2014-01-05 10:44:53 -0500

    Storage configuration options (2/2)
    Change-Id: Ie487323edce58f174b4956a2e832ac4c121309ef

--- src/com/android/gallery3d/util/MediaSetUtils.java
+++ src/com/android/gallery3d/util/MediaSetUtils.java
@@ -28,9 +28,12 @@ import java.util.Comparator;
 public class MediaSetUtils {
     public static final Comparator<MediaSet> NAME_COMPARATOR = new NameComparator();
 
-    public static final int CAMERA_BUCKET_ID = GalleryUtils.getBucketId(
-            Environment.getExternalStorageDirectory().toString() + "/"
-            + BucketNames.CAMERA);
+    private static String mRoot = Environment.getExternalStorageDirectory().toString();
+
+    public static void setRoot(String root) {
+        mRoot = root;
+    }
+
     public static final int DOWNLOAD_BUCKET_ID = GalleryUtils.getBucketId(
             Environment.getExternalStorageDirectory().toString() + "/"
             + BucketNames.DOWNLOAD);
@@ -44,14 +47,14 @@ public class MediaSetUtils {
             Environment.getExternalStorageDirectory().toString() +
             "/" + BucketNames.SCREENSHOTS);
 
-    private static final Path[] CAMERA_PATHS = {
-            Path.fromString("/local/all/" + CAMERA_BUCKET_ID),
-            Path.fromString("/local/image/" + CAMERA_BUCKET_ID),
-            Path.fromString("/local/video/" + CAMERA_BUCKET_ID)};
+    public static int getCameraBucketId() {
+        return GalleryUtils.getBucketId(mRoot + "/" + BucketNames.CAMERA);
+    }
 
     public static boolean isCameraSource(Path path) {
-        return CAMERA_PATHS[0] == path || CAMERA_PATHS[1] == path
-                || CAMERA_PATHS[2] == path;
+        return path.equalsIgnoreCase("/local/all/" + getCameraBucketId())
+                || path.equalsIgnoreCase("/local/image/" + getCameraBucketId())
+                || path.equalsIgnoreCase("/local/video/" + getCameraBucketId());
     }
 
     // Sort MediaSets by name
