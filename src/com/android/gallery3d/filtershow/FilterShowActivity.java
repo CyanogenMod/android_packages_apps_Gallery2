d29495516bccd5bb47722051b4e8ce51da2d1328
Suman Mukherjee <sumam@codeaurora.org>
2014-01-07 09:30:00 +0530

    Camera: Ensure background edition complete on back Key
    Block back key event till the image will get displayed
    while user pressed edit option/icon. This'll allow the
    background process to cleanup memory. Added null check
    and bitmap recycle to avoid crash.
    
    Change-Id: Ied1c1f3ecf07a352743908135a5b7976373a58a3
    CRs-Fixed:  587402

--- src/com/android/gallery3d/filtershow/FilterShowActivity.java
+++ src/com/android/gallery3d/filtershow/FilterShowActivity.java
@@ -195,6 +195,7 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
     private DialogInterface mCurrentDialog = null;
     private PopupMenu mCurrentMenu = null;
     private boolean mLoadingVisible = true;
+    private boolean mLoadingComplete = false;
 
     public ProcessingService getProcessingService() {
         return mBoundService;
@@ -283,6 +284,7 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
         doBindService();
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
         setContentView(R.layout.filtershow_splashscreen);
+        mLoadingComplete = false;
     }
 
     public boolean isShowingImageStatePanel() {
@@ -861,6 +863,7 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
 
             MasterImage.getImage().warnListeners();
             loadActions();
+            mLoadingComplete = false;
 
             if (mOriginalPreset != null) {
                 MasterImage.getImage().setLoadedPreset(mOriginalPreset);
@@ -1162,6 +1165,7 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
     }
 
     public void enableSave(boolean enable) {
+        mLoadingComplete = true;
         if (mSaveButton != null) {
             mSaveButton.setEnabled(enable);
         }
@@ -1329,6 +1333,10 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
         Fragment currentPanel = getSupportFragmentManager().findFragmentByTag(MainPanel.FRAGMENT_TAG);
         if (currentPanel instanceof MainPanel) {
             if (!mImageShow.hasModifications()) {
+                if (!mLoadingComplete) {
+                    Log.v(LOGTAG,"Background processing is ON, rejecting back key event");
+                    return;
+                }
                 done();
             } else {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
