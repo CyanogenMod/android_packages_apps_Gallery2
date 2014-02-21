d40209901d3ccc182a3cec37e764ada1460c39b6
Likai Ding <likaid@codeaurora.org>
2014-02-21 11:22:32 +0800

    MoviePlayer: fix streaming video stepping forward
    The default forward/rewind step is 3/6 seconds, which falls short to
    reach another intra-frame. Change the default to 10 seconds.
    Also, some code clean-up is made.
    
    CRs-Fixed: 613577
    
    Change-Id: I94bceb5cdac0904545ef58609855c8fb8d6aa05a

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -63,6 +63,7 @@ import org.codeaurora.gallery3d.video.IControllerRewindAndForward.IRewindAndForw
 import org.codeaurora.gallery3d.video.ScreenModeManager;
 import org.codeaurora.gallery3d.video.ScreenModeManager.ScreenModeListener;
 import org.codeaurora.gallery3d.video.CodeauroraVideoView;
+import org.codeaurora.gallery3d.video.VideoSettingsActivity;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -1525,7 +1526,7 @@ public class MoviePlayer implements
                 mControllerRewindAndForwardExt.showControllerButtonsView(mPlayerExt
                         .canStop(),
                         false, false);
-                int stepValue = getStepOptionValue();
+                int stepValue = VideoSettingsActivity.getStepOptionValue(mContext);
                 int targetDuration = mVideoView.getCurrentPosition()
                         - stepValue < 0 ? 0 : mVideoView.getCurrentPosition()
                         - stepValue;
@@ -1545,7 +1546,7 @@ public class MoviePlayer implements
                 mControllerRewindAndForwardExt.showControllerButtonsView(mPlayerExt
                         .canStop(),
                         false, false);
-                int stepValue = getStepOptionValue();
+                int stepValue = VideoSettingsActivity.getStepOptionValue(mContext);
                 int targetDuration = mVideoView.getCurrentPosition()
                         + stepValue > mVideoView.getDuration() ? mVideoView
                         .getDuration() : mVideoView.getCurrentPosition()
@@ -1558,16 +1559,6 @@ public class MoviePlayer implements
                         false, false);
             }
         }
-    }
-
-    public int getStepOptionValue() {
-        final String slectedStepOption = "selected_step_option";
-        final String videoPlayerData = "video_player_data";
-        final int stepBase = 3000;
-        final int stepOptionThreeSeconds = 0;
-        SharedPreferences mPrefs = mContext.getSharedPreferences(
-                videoPlayerData, 0);
-        return (mPrefs.getInt(slectedStepOption, stepOptionThreeSeconds) + 1) * stepBase;
     }
 }
 
