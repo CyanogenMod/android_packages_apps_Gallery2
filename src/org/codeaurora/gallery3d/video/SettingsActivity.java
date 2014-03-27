a1f2e8ec818fc6f06b974bcce8bdfc85e1635cc9
kaiyiz <kaiyiz@codeaurora.org>
2014-03-27 12:09:52 +0800

    Gallery: add an activity to set system properties for streaming
    App forgets add an activity to set system properties for streaming.
    
    add an activity to set system properties for streaming.
    
    CRs-Fixed: 635639
    
    Change-Id: Idba31bd7a7489a01b5cc061fffb4ebcc7af3f813

--- src/org/codeaurora/gallery3d/video/SettingsActivity.java
+++ src/org/codeaurora/gallery3d/video/SettingsActivity.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of The Linux Foundation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.codeaurora.gallery3d.video;
 
 import android.app.ActionBar;
@@ -204,7 +233,6 @@ public class SettingsActivity extends PreferenceActivity {
                 Integer.toString(DEFAULT_RTP_MINPORT));
         final String rtpMaxPortStr = mPref.getString(PREFERENCE_RTP_MAXPORT,
                 Integer.toString(DEFAULT_RTP_MAXPORT));
-        final String CLASS_NAME    = "com.android.settings.StreamingSettingsEnablerActivity";
         final int rtpMinPort;
         final int rtpMaxPort;
         try {
@@ -215,7 +243,7 @@ public class SettingsActivity extends PreferenceActivity {
             return;
         }
         Intent intent = new Intent();
-        intent.setClassName(PACKAGE_NAME, CLASS_NAME);
+        intent.setClass(this,StreamingSettingsEnablerActivity.class);
         intent.putExtra(PREFERENCE_RTP_MINPORT, rtpMinPort);
         intent.putExtra(PREFERENCE_RTP_MAXPORT, rtpMaxPort);
         startActivity(intent);
@@ -224,7 +252,6 @@ public class SettingsActivity extends PreferenceActivity {
     private void enableBufferSetting() {
         final String bufferSizeStr = mPref.getString(PREFERENCE_BUFFER_SIZE, 
                 Integer.toString(DEFAULT_CACHE_MAX_SIZE));
-        final String CLASS_NAME    = "com.android.settings.StreamingSettingsEnablerActivity";
         final int cacheMaxSize;
         try {
             cacheMaxSize = Integer.valueOf(bufferSizeStr);
@@ -233,7 +260,7 @@ public class SettingsActivity extends PreferenceActivity {
             return;
         }
         Intent intent = new Intent();
-        intent.setClassName(PACKAGE_NAME, CLASS_NAME);
+        intent.setClass(this,StreamingSettingsEnablerActivity.class);
         intent.putExtra(PREFERENCE_CACHE_MIN_SIZE, DEFAULT_CACHE_MIN_SIZE);
         intent.putExtra(PREFERENCE_CACHE_MAX_SIZE, cacheMaxSize);
         intent.putExtra(PREFERENCE_KEEP_ALIVE_INTERVAL_SECOND, DEFAULT_KEEP_ALIVE_INTERVAL_SECOND);
