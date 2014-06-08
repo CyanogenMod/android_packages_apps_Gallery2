3ec757d83ab83c574f5d3ce0111429633845ddf5
Vladislav Koldobskiy <admin@nevergone.ru>
2014-06-08 23:26:44 +0400

    Make Camera2 DCIM-compliant [2/2]
    According to DCF specification (http://www.schnarff.com/file-formats/exif/dcf.PDF),
    directory names in DCIM should follow the following conventions:
    ∙ The directory name is 8 characters in length.
    ∙ The first three characters are a number between "100" and "999"; numbers "000"
    through "099" shall not be used. This number is referred to as the directory
    number.
    ∙ The rest of the name after the directory number shall be five characters. These are
    referred to here as the DCF directory name Free characters.
    ∙ These five characters consist only of the upper-case alphanumeric characters
    shown in Table 1-1. They shall not contain two-byte characters or special codes.
    
    This is a second part that makes Gallery properly display "DCIM/100MEDIA" as a
    "Camera" album.
    
    Change-Id: I526b99a0ec57ed0f1f4509341db6618f32a1168c

--- src/com/android/gallery3d/util/BucketNames.java
+++ src/com/android/gallery3d/util/BucketNames.java
@@ -21,7 +21,7 @@ package com.android.gallery3d.util;
  */
 public class BucketNames {
 
-    public static final String CAMERA = "DCIM/Camera";
+    public static final String CAMERA = "DCIM/100MEDIA";
     public static final String IMPORTED = "Imported";
     public static final String DOWNLOAD = "download";
     public static final String EDITED_ONLINE_PHOTOS = "EditedOnlinePhotos";
