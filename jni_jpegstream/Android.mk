745eb589234bac233ce62a612771b77eddbb3b89
Ying Wang <wangying@google.com>
2014-07-22 12:27:43 -0700

    Build against the NDK libraries.
    That way you don't have dependency on the platform libraries, which in
    turn depends on things like libcompiler_rt-extras, which isn't exposed
    to apps_only build.
    
    Bug: 16398512
    Change-Id: Ie7cf54c97c57b13e82c1891970eb3731a68ae075

--- jni_jpegstream/Android.mk
+++ jni_jpegstream/Android.mk
@@ -12,14 +12,14 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/src \
                     external/jpeg
 
-LOCAL_SHARED_LIBRARIES := libjpeg liblog
+LOCAL_SHARED_LIBRARIES := libjpeg
 
 LOCAL_SDK_VERSION   := 9
 LOCAL_ARM_MODE := arm
 
 LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
 LOCAL_CPPFLAGS += $(JNI_CFLAGS)
-
+LOCAL_LDLIBS := -llog
 
 LOCAL_CPP_EXTENSION := .cpp
 LOCAL_SRC_FILES     := \
