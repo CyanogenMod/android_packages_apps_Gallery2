49ca5f7e4f553dd9bb96fc99c95904a71ff106f6
kaiyiz <kaiyiz@codeaurora.org>
2015-01-28 14:55:30 +0800

    Gallery2: Set PhotoPage GL root reset when rotate
    When there is another activity on top of PhotoPage, the GLRootView
    will not update to match screen, so the back ground seems corruption.
    
    Set GLRoot View resume when onConfigurationChanged happen.
    
    CRs-Fixed: 778294
    
    Change-Id: Ib1fd6953462e26f11c3a8957a042db2ad4ecc275

--- src/com/android/gallery3d/app/PhotoPage.java
+++ src/com/android/gallery3d/app/PhotoPage.java
@@ -1631,6 +1631,15 @@ public abstract class PhotoPage extends ActivityState implements
     }
 
     @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+        if(mIsActive) return;
+        mActivity.GLRootResume(true);
+        mModel.resume();
+        mActivity.GLRootResume(false);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
 
