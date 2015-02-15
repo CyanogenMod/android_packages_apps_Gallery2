b7231bd9b432a62bf8ccb63af7219869ab927c8c
kaiyiz <kaiyiz@codeaurora.org>
2015-02-15 13:38:20 +0800

    Gallery2: Translate image draw to Chinese
    The name of FilterDrawRepresentation is set 'Draw' constant.
    
    Set name of FilterDrawRepresentation from imageDraw in filter_string.
    
    Change-Id: If4ee1c4e96e52cd267c90afde1081d5b68f4d206
    CRs-Fixed: 794963

--- src/com/android/gallery3d/filtershow/FilterShowActivity.java
+++ src/com/android/gallery3d/filtershow/FilterShowActivity.java
@@ -483,7 +483,8 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
             }
         }
         if (!found) {
-            FilterRepresentation representation = new FilterDrawRepresentation();
+            FilterRepresentation representation =
+                    new FilterDrawRepresentation(getString(R.string.imageDraw));
             Action action = new Action(this, representation);
             action.setIsDoubleAction(true);
             mCategoryGeometryAdapter.add(action);
