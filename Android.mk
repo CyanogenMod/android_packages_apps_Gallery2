d1934db9dff646dc27c8dd514968e7f08b1e1ae8
cretin45 <cretin45@gmail.com>
2015-04-30 15:34:55 -0700

    Gallery2: Use a slimmed down manifest on .inc builds
    Change-Id: I482c46947397a5ffb7e7086088bfab1c1c0c6b65

--- Android.mk
+++ Android.mk
@@ -34,6 +34,12 @@ ifeq ($(TARGET_TS_MAKEUP), true)
   LOCAL_REQUIRED_MODULES := libts_detected_face_jni libts_face_beautify_jni
 endif
 
+ifeq ($(WITH_SLIM_GALLERY2), true)
+  LOCAL_MANIFEST_FILE := slim_manifest/AndroidManifest.xml
+else
+  LOCAL_MANIFEST_FILE := AndroidManifest.xml
+endif
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_JAVA_LIBRARIES += org.apache.http.legacy
