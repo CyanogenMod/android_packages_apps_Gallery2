cf002f22904821fb603e6bb625c50159ddc5d8bb
wangjing <wangjing@codeaurora.org>
2015-07-27 09:36:57 +0800

    Gallery2: invalidate album view even only size changed
    After deleting the pictures, it didn't request to
    update the view. so the pictures can not be
    updated and displayed instantly.
    
    Invalidate album view even only size changed to
    make it shown correctly.
    
    Change-Id: I2a5a3fc7a9cfdd0a869786324cc430a6cffd12e0
    CRs-Fixed: 878280

--- src/com/android/gallery3d/ui/AlbumSlotRenderer.java
+++ src/com/android/gallery3d/ui/AlbumSlotRenderer.java
@@ -170,6 +170,7 @@ public class AlbumSlotRenderer extends AbstractSlotRenderer {
         @Override
         public void onSizeChanged(int size) {
             mSlotView.setSlotCount(size);
+            mSlotView.invalidate();
         }
     }
 
