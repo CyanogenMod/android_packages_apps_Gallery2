189c50a6cbf7b5bb5be0cc58729c5ec787f4f95f
AUDITYA BHATTARAM <audityab@codeaurora.org>
2015-08-17 15:02:52 +0530

    Disable progaurd flags to support java compilation.
    Disable progaurd flags to support java compilation
    instead of jack & jill compilation.
    
    Change-Id: I4c2b5296cb4dbcf4292d03d2b474039df3c9cb09

--- Android.mk
+++ Android.mk
@@ -31,6 +31,8 @@ LOCAL_PACKAGE_NAME := Gallery2
 
 LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 
+LOCAL_PROGUARD_ENABLED := disabled
+
 LOCAL_SDK_VERSION := current
 
 LOCAL_JNI_SHARED_LIBRARIES := libjni_eglfence libjni_filtershow_filters librsjni libjni_jpegstream
