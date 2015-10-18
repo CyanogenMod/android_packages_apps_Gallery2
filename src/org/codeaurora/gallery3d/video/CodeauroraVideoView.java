c1330ec0c5ef804beef398a2608c751eb89a2489
Steve Kondik <steve@cyngn.com>
2015-10-18 12:45:07 -0700

    Revert "Gallery2: Enable Suspend/Resume"
    This reverts commit 9b1ab3613c9e22f45c7404d5a5acd6b24df4dce6.
    
    Change-Id: Ic023655b4dd47be35cf5efa43557af1c60cb1420

--- src/org/codeaurora/gallery3d/video/CodeauroraVideoView.java
+++ src/org/codeaurora/gallery3d/video/CodeauroraVideoView.java
@@ -371,6 +371,7 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
                 if (LOG) {
                     Log.v(TAG, "surfaceCreated(" + holder + ")");
                 }
+                /*
                 if (mCurrentState == STATE_SUSPENDED) {
                     mSurfaceHolder = holder;
                     mMediaPlayer.setDisplay(mSurfaceHolder);
@@ -386,6 +387,12 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
                     } else {
                         release(false);
                     }
+                }
+                */
+                if (mCurrentState == STATE_SUSPENDED) {
+                    mSurfaceHolder = holder;
+                    mMediaPlayer.setDisplay(mSurfaceHolder);
+                    release(false);
                 }
                 mSurfaceHolder = holder;
                 openVideo();
@@ -623,9 +630,7 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
         }
 
         public void surfaceCreated(SurfaceHolder holder) {
-            if (LOG) {
-                Log.v(TAG, "surfaceCreated(" + holder + ")");
-            }
+            /*
             if (mCurrentState == STATE_SUSPENDED) {
                 mSurfaceHolder = holder;
                 mMediaPlayer.setDisplay(mSurfaceHolder);
@@ -641,6 +646,17 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
                 } else {
                     release(false);
                 }
+            }
+            mSurfaceHolder = holder;
+            openVideo();
+            */
+            if (LOG) {
+                Log.v(TAG, "surfaceCreated(" + holder + ")");
+            }
+            if (mCurrentState == STATE_SUSPENDED) {
+                mSurfaceHolder = holder;
+                mMediaPlayer.setDisplay(mSurfaceHolder);
+                release(false);
             }
             mSurfaceHolder = holder;
             openVideo();
@@ -773,6 +789,7 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
     }
 
     public void suspend() {
+        /*
         // HTTP streaming will call mMediaPlayer->suspend(), others will call release()
         if (isHTTPStreaming(mUri) && mCurrentState != STATE_PREPARING) {
             if (mMediaPlayer != null) {
@@ -782,13 +799,13 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
                     return;
                 }
             }
-        }
+        }*/
         release(false);
     }
 
     public void resume() {
-        // HTTP streaming (with suspended status) will call mMediaPlayer->resume(), 
-        // others will call openVideo()
+        /*
+        // HTTP streaming (with suspended status) will call mMediaPlayer->resume(), others will call openVideo()
         if (mCurrentState == STATE_SUSPENDED) {
             if (mSurfaceHolder != null) {
                 // The surface hasn't been destroyed
@@ -806,8 +823,16 @@ public class CodeauroraVideoView extends SurfaceView implements MediaPlayerContr
                      release(false);
                 }
             } else {
-                // The surface has been destroyed, resume operation will be done 
-                // after surface created
+                // The surface has been destroyed, resume operation will be done after surface created
+                return;
+            }
+        }*/
+        // HTTP streaming (with suspended status) will call mMediaPlayer->resume(), others will call openVideo()
+        if (mCurrentState == STATE_SUSPENDED) {
+            if (mSurfaceHolder != null) {
+                 release(false);
+            } else {
+                // The surface has been destroyed, resume operation will be done after surface created
                 return;
             }
         }
