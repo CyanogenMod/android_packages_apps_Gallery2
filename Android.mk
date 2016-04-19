bde96829065bbbeb0620f072a4ae0657a0300ca1
Chao Zhang <chaoz@codeaurora.org>
2016-04-19 10:43:08 +0800

    Gallery2: set module privileged in Android.mk to delete photos on SD card.
    set Gallery2 module privileged in Android.mk,
    make sure Gallery2 has permission to delete photos on SD card.
    
    Change-Id: I65aed83b3cea951202c001e556d9e4200f2bc81f
    CRs-Fixed: 990742

--- Android.mk
+++ Android.mk
@@ -22,6 +22,8 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 LOCAL_PACKAGE_NAME := Gallery2
 
+LOCAL_PRIVILEGED_MODULE := true
+
 LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 
 LOCAL_PROGUARD_ENABLED := disabled
