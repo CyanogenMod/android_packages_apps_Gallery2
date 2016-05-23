698e3912a146078b18976e61094173b1d3de6991
Michael W <baddaemon87@gmail.com>
2016-05-23 13:17:28 +0200

    GalleryCAF: Try to open existing camera
    Do not just open Camera2, just open any existing cam
    Also get rid of context parameter as it's unused
    
    (Reference: BugReports 13-20160506-12 Line #5)
    
    Change-Id: I120a1561aeceda2fc23721aa2ff4d68710fab76c

--- src_pd/com/android/gallery3d/util/IntentHelper.java
+++ src_pd/com/android/gallery3d/util/IntentHelper.java
@@ -15,17 +15,16 @@
  */
 package com.android.gallery3d.util;
 
-import android.content.Context;
 import android.content.Intent;
+import android.provider.MediaStore;
 
 public class IntentHelper {
 
-    public static Intent getCameraIntent(Context context) {
-        return new Intent(Intent.ACTION_MAIN)
-            .setClassName("com.android.camera2", "com.android.camera.CameraLauncher");
+    public static Intent getCameraIntent() {
+        return new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
     }
 
-    public static Intent getGalleryIntent(Context context) {
+    public static Intent getGalleryIntent() {
         return new Intent(Intent.ACTION_MAIN)
             .setClassName("com.android.gallery3d", "com.android.gallery3d.app.GalleryActivity");
     }
