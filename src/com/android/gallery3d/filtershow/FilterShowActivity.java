c07b5989906130e2f5c24f93788243411a3c5a5d
wqi <wqi@codeaurora.org>
2016-06-02 15:47:02 +0800

    Gallery2: Fix NullPointerException when edit picture.
    Add NullPointerException check of mMasterImage.
    
    Change-Id: I278f95d96bd83f2683fc96455e06301e2e1aa3f7
    CRs-Fixed: 1005204

--- src/com/android/gallery3d/filtershow/FilterShowActivity.java
+++ src/com/android/gallery3d/filtershow/FilterShowActivity.java
@@ -1311,6 +1311,9 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
     }
 
     void resetHistory() {
+        if (mMasterImage == null) {
+            return;
+        }
         HistoryManager adapter = mMasterImage.getHistory();
         adapter.reset();
         HistoryItem historyItem = adapter.getItem(0);
