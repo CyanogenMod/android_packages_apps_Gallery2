fb7f4481a1d6ded32bc3a29d7683cd90bf24b2fc
Dan Albert <danalbert@google.com>
2016-07-26 15:05:35 -0700

    Cleanup warnings in jni.
    Test: mma
    Change-Id: I1a3997525427258d628a7c59528c758070ab5f82

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS += -DEGL_EGLEXT_PROTOTYPES
+LOCAL_CFLAGS += -DEGL_EGLEXT_PROTOTYPES -Wno-unused-parameter
 
 LOCAL_SRC_FILES := jni_egl_fence.cpp
 
@@ -44,7 +44,7 @@ LOCAL_SRC_FILES := filters/gradient.c \
                    filters/tinyplanet.cc \
                    filters/kmeans.cc
 
-LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
+LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops -Wno-unused-parameter
 LOCAL_LDLIBS := -llog -ljnigraphics
 LOCAL_ARM_MODE := arm
 
