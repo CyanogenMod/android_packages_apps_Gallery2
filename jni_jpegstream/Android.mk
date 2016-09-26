f17a360481a3655e2d67be64a88faecddc3e52a6
Steve Kondik <steve@cyngn.com>
2016-09-26 04:27:52 -0700

    Fix jni dependencies
    Change-Id: If7309802e0688cba8334cf9cf8ba7e99f3594d5d

--- jni_jpegstream/Android.mk
+++ jni_jpegstream/Android.mk
@@ -6,14 +6,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE        := libjni_jpegstream
 
-LOCAL_NDK_STL_VARIANT := stlport_static
-
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/src
 
-LOCAL_STATIC_LIBRARIES := libjpeg_static_ndk
+LOCAL_SHARED_LIBRARIES := libjpeg
 
-LOCAL_SDK_VERSION   := 17
 LOCAL_ARM_MODE := arm
 
 LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
